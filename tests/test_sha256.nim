fed80688bee35713bb61d4a8c695753e5af5cfb7
Ryan Oldenburg <ryan@guzba.com>
2023-01-12 21:37:28 -0600

    add hmac-sha256 and pbkdf2

--- tests/test_sha256.nim
+++ tests/test_sha256.nim
@@ -25,3 +25,36 @@ const sha256Tests = [
 
 for (a, b) in sha256Tests:
   doAssert sha256(a).toHex() == b
+
+const hmacSha256Tests = [
+  (
+    "abc",
+    "def",
+    "20ebc0f09344470134f35040f63ea98b1d8e414212949ee5c500429d15eab081"
+  ),
+  (
+    "asdf3q5q23rfasf3a",
+    "dfasdfasfd3qr3fqfaefa3fa3rfasadfasfdasdfasdfasdfasfdasd",
+    "a1629e21b02776e7eacef6f615165d08894963a12dfbd304a47e7a8aff0a2dc5"
+  ),
+  (
+    "awjieops;oi4etaawjieops;oi4etaawjieops;oi4etaawjieops;oi4etaawjieops;oi4",
+    "p890y6t3q9ah2pqh8t6q3pth8qa3whu",
+    "a1b7d6b597ae74f950dad4eb4dc1700420281b186abfaa321f728f38d675b099"
+  )
+]
+
+for (a, b, c) in hmacSha256Tests:
+  doAssert hmacSha256(a, b).toHex() == c
+
+const pbkdf2Tests = [
+  (
+    "password",
+    "salt",
+    30000,
+    "76639c203f99c73c1151d8dee2ca9d5055c932a2b0a4708c10dc21b60c921ca7"
+  )
+]
+
+for (a, b, c, d) in pbkdf2Tests:
+  doAssert pbkdf2(a, b, c).toHex() == d
