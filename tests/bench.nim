a3c4e994116a212d068e5cf500e003118b216196
Ryan Oldenburg <ryan@guzba.com>
2023-01-19 19:37:05 -0600

    f

--- tests/bench.nim
+++ tests/bench.nim
@@ -6,25 +6,25 @@ var data = newString(10_000_000)
 for c in data.mitems:
   c = rand(0 .. 255).char
 
-# timeIt "crc32":
-#   discard crc32(data)
+timeIt "crc32":
+  discard crc32(data)
 
-# when allowSimd:
-#   timeIt "crc32c":
-#     discard crc32c(data)
+when allowSimd:
+  timeIt "crc32c":
+    discard crc32c(data)
 
-# timeIt "adler32":
-#   discard adler32(data)
+timeIt "adler32":
+  discard adler32(data)
 
-# timeIt "sha256":
-#   discard sha256(data)
+timeIt "sha256":
+  discard sha256(data)
 
-# block:
-#   var
-#     key: array[32, uint8]
-#     iv: array[16, uint8]
-#   timeIt "rc6cbc":
-#     discard rc6cbcDecrypt(key, iv, rc6cbcEncrypt(key, iv, data))
+block:
+  var
+    key: array[32, uint8]
+    iv: array[16, uint8]
+  timeIt "rc6cbc":
+    discard rc6cbcDecrypt(key, iv, rc6cbcEncrypt(key, iv, data))
 
 block:
   var
@@ -32,4 +32,4 @@ block:
     iv: array[12, uint8]
   timeIt "aes256gcm":
     let (encrypted, _) = aes256gcmEncrypt(key, iv, data)
-    # discard aes256gcmDecrypt(key, iv, encrypted)
+    discard aes256gcmDecrypt(key, iv, encrypted)
