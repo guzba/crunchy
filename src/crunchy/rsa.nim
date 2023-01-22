7cbe11052d4d257ccacfddd733c3f84122dd44d6
Ryan Oldenburg <ryan@guzba.com>
2023-01-21 22:52:11 -0600

    rsa

---
+++ src/crunchy/rsa.nim
@@ -0,0 +1,241 @@
+import bigints, common, sha256, std/base64, std/strutils
+
+export bigints
+
+type RsaPrivateKey* = object
+  size*: int ## In bits, eg 1024, 2048, 4096
+  n*, e*, d*, p*, q*, e1*, e2*, coef*: BigInt
+
+proc sign*(pk: RsaPrivateKey, message: string): string =
+  ## RSASSA-PKCS1-v1_5 using SHA-256
+
+  const oid =
+    "\x30\x31\x30\x0d\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x01\x05\x00\x04\x20"
+
+  let hash = sha256(message)
+
+  var padded: string
+
+  case pk.size:
+  of 1024:
+    padded.setLen(128)
+  of 2048:
+    padded.setLen(256)
+  of 4096:
+    padded.setLen(512)
+  else:
+    raise newException(CrunchyError, "Unexpected RSA key size: " & $pk.size)
+
+  padded[1] = 1.char
+
+  for i in 2 ..< padded.len - 32 - oid.len - 1:
+    padded[i] = 0xff.char
+
+  let oidStart = padded.len - 32 - oid.len
+  for i in 0 ..< oid.len:
+    padded[oidStart + i] = oid[i]
+
+  copyMem(padded[padded.len - 32].addr, hash[0].unsafeAddr, 32)
+
+  result = initBigInt(padded.toHex(), base = 16).powmod(pk.d, pk.n).toString(16)
+
+template raisePrivateKeyError() =
+  raise newException(CrunchyError, "RSA private key data appears invalid")
+
+proc decodeLength(buf: string, pos: var int): int =
+  # https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-integer
+  if (buf[pos].uint8 and 0b10000000) != 0:
+    let numBytes = (buf[pos].uint8 and 0b01111111).int
+    inc pos
+    if pos + numBytes > buf.len or  numBytes > 4:
+      raisePrivateKeyError()
+    for _ in 0 ..< numBytes:
+      result = (result shl 8) or buf[pos].int
+      inc pos
+  else:
+    result = buf[pos].int
+    inc pos
+
+proc decodeBigInt(buf: string, pos: var int): BigInt =
+  let len = decodeLength(buf, pos)
+  if pos + len > buf.len:
+    raisePrivateKeyError()
+  result = initBigInt(buf[pos ..< pos + len].toHex(), base = 16)
+  pos += len
+
+proc decodePrivateKey*(s: string): RsaPrivateKey =
+  var lines = s.split('\n')
+  if lines[0].startsWith('-'):
+    # Trim off -----BEGIN RSA PRIVATE KEY-----, -----END RSA PRIVATE KEY-----
+    lines = lines[1 ..< ^2]
+
+  let buf = decode(lines.join())
+
+  var pos: int
+
+  if pos + 2 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x30.char:
+    raisePrivateKeyError()
+  inc pos
+
+  discard decodeLength(buf, pos)
+
+  if pos + 5 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x02.char:
+    raisePrivateKeyError()
+  inc pos
+
+  if buf[pos] != 0x01.char:
+    raisePrivateKeyError()
+  inc pos
+
+  if buf[pos] != 0x00.char:
+    raisePrivateKeyError()
+  inc pos
+
+  # This is PKCS#8 format, skip past the header
+  if buf[pos] == 0x30.char:
+    inc pos
+
+    discard decodeLength(buf, pos)
+
+    if pos + 2 > buf.len:
+      raisePrivateKeyError()
+
+    if buf[pos] != 0x06.char:
+      raisePrivateKeyError()
+    inc pos
+
+    # https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-object-identifier
+
+    let oidLen = decodeLength(buf, pos)
+
+    if pos + oidLen > buf.len:
+      raisePrivateKeyError()
+
+    pos += oidLen
+
+    if pos + 4 > buf.len:
+      raisePrivateKeyError()
+
+    if buf[pos] != 0x05.char:
+      raisePrivateKeyError()
+    inc pos
+
+    if buf[pos] != 0x00.char:
+      raisePrivateKeyError()
+    inc pos
+
+    if buf[pos] != 0x04.char:
+      raisePrivateKeyError()
+    inc pos
+
+    discard decodeLength(buf, pos)
+
+    if pos + 2 > buf.len:
+      raisePrivateKeyError()
+
+    if buf[pos] != 0x30.char:
+      raisePrivateKeyError()
+    inc pos
+
+    discard decodeLength(buf, pos)
+
+    if pos + 3 > buf.len:
+      raisePrivateKeyError()
+
+    if buf[pos] != 0x02.char:
+      raisePrivateKeyError()
+    inc pos
+
+    if buf[pos] != 0x01.char:
+      raisePrivateKeyError()
+    inc pos
+
+    if buf[pos] != 0x00.char:
+      raisePrivateKeyError()
+    inc pos
+
+  # PKCS#1 private key values start here
+
+  if pos + 2 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x02.char:
+    raisePrivateKeyError()
+  inc pos
+
+  block:
+    let
+      tmp = pos
+      len = decodeLength(buf, pos)
+    pos = tmp
+    result.size = ((len div 8) * 8) * 8
+    result.n = decodeBigInt(buf, pos)
+
+  if pos + 2 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x02.char:
+    raisePrivateKeyError()
+  inc pos
+
+  result.e = decodeBigInt(buf, pos)
+
+  if pos + 2 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x02.char:
+    raisePrivateKeyError()
+  inc pos
+
+  result.d = decodeBigInt(buf, pos)
+
+  if pos + 2 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x02.char:
+    raisePrivateKeyError()
+  inc pos
+
+  result.p = decodeBigInt(buf, pos)
+
+  if pos + 2 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x02.char:
+    raisePrivateKeyError()
+  inc pos
+
+  result.q = decodeBigInt(buf, pos)
+
+  if pos + 2 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x02.char:
+    raisePrivateKeyError()
+  inc pos
+
+  result.e1 = decodeBigInt(buf, pos)
+
+  if pos + 2 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x02.char:
+    raisePrivateKeyError()
+  inc pos
+
+  result.e2 = decodeBigInt(buf, pos)
+
+  if pos + 2 > buf.len:
+    raisePrivateKeyError()
+
+  if buf[pos] != 0x02.char:
+    raisePrivateKeyError()
+  inc pos
+
+  result.coef = decodeBigInt(buf, pos)
--- tests/bench.nim
+++ tests/bench.nim
@@ -1,35 +1,41 @@
-import benchy, crunchy, crunchy/internal, std/random, crunchy/rc6, crunchy/aes256
+import benchy, crunchy, crunchy/internal, std/random, crunchy/rc6, crunchy/aes256,
+    crunchy/rsa
 
-randomize()
+# randomize()
 
-var data = newString(10_000_000)
-for c in data.mitems:
-  c = rand(0 .. 255).char
+# var data = newString(10_000_000)
+# for c in data.mitems:
+#   c = rand(0 .. 255).char
 
-timeIt "crc32":
-  discard crc32(data)
+# timeIt "crc32":
+#   discard crc32(data)
 
-when allowSimd:
-  timeIt "crc32c":
-    discard crc32c(data)
+# when allowSimd:
+#   timeIt "crc32c":
+#     discard crc32c(data)
 
-timeIt "adler32":
-  discard adler32(data)
+# timeIt "adler32":
+#   discard adler32(data)
 
-timeIt "sha256":
-  discard sha256(data)
+# timeIt "sha256":
+#   discard sha256(data)
+
+# block:
+#   var
+#     key: array[32, uint8]
+#     iv: array[16, uint8]
+#   timeIt "rc6cbc":
+#     discard rc6cbcDecrypt(key, iv, rc6cbcEncrypt(key, iv, data))
+
+# block:
+#   var
+#     key: array[32, uint8]
+#     iv: array[12, uint8]
+#   timeIt "aes256gcm":
+#     let (encrypted, _) = aes256gcmEncrypt(key, iv, data)
+#     discard aes256gcmDecrypt(key, iv, encrypted)
 
 block:
-  var
-    key: array[32, uint8]
-    iv: array[16, uint8]
-  timeIt "rc6cbc":
-    discard rc6cbcDecrypt(key, iv, rc6cbcEncrypt(key, iv, data))
-
-block:
-  var
-    key: array[32, uint8]
-    iv: array[12, uint8]
-  timeIt "aes256gcm":
-    let (encrypted, _) = aes256gcmEncrypt(key, iv, data)
-    discard aes256gcmDecrypt(key, iv, encrypted)
+  let pk = decodePrivateKey(readFile("/Users/me/Documents/GitHub/crunchy/tests/data/2048.txt"))
+  timeIt "powmod":
+    discard pk.p.powmod(pk.d, pk.n)
